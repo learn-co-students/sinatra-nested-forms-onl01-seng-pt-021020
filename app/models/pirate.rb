574728cfa4762cff036454051dc2b3af8cf22186
pbedrosian <peter_bedrosian@Yahoo.com>
2020-05-08 11:54:46 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+    attr_accessor :name, :height, :weight
+
+    @@all = []
+
+    def initialize(hash)
+        @name = hash[:name]
+        @height = hash[:height]
+        @weight = hash[:weight]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
