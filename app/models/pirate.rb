952c9da581f9806db1476f774f51c72f34823273
TerryThreatt <terry.threatt@gmail.com>
2020-05-10 16:14:57 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
-end
+
+    attr_accessor :name, :weight, :height
+
+    @@pirates = []
+
+    def initialize(params)
+      @name = params[:name]
+      @weight = params[:weight]
+      @height = params[:height]
+      @@pirates << self
+    end
+
+    def self.all
+      @@pirates
+    end
+
+  end
